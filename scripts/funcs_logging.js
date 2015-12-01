5742aee8b619915500b46333c469200b2ce1a0e0
Walter Lasecki <wslasecki@gmail.com>
2015-11-30 23:47:31 -0500

    file reorg

--- scripts/funcs_logging.js
+++ scripts/funcs_logging.js
@@ -2,7 +2,7 @@
 
 function logTrial(worker, trial, session, frametime, speed, starttime, duration, avrgproximity, misses, targetsmissed) {
   $.ajax({
-    url: "log_trial.php",
+    url: "php/log_trial.php",
     data: {worker: worker, trial: trial, session: session, frametime: frametime, speed: speed, starttime: starttime, duration: duration, avrgproximity: avrgproximity, misses:misses, targetsmissed: targetsmissed},
     dataType: "text",
     success: function(d) {
@@ -13,7 +13,7 @@ function logTrial(worker, trial, session, frametime, speed, starttime, duration,
 
 function logTarget(worker, trial, session, frametime, speed, targetindex, starttime, duration, startloc, endloc, path, distance, proximity, misses) {
   $.ajax({
-    url: "log_target.php",
+    url: "php/log_target.php",
     data: {worker: worker, trial: trial, session: session, frametime: frametime, speed: speed, targetindex: targetindex, starttime: starttime, duration: duration, startloc: startloc, endloc: endloc, path: path, distance: distance, proximity: proximity, misses: misses},
     dataType: "text",
     success: function(d) {
