aef3c60cd17bd7201a82f351dfc81a8283839386
Walter Lasecki <wslasecki@gmail.com>
2015-12-01 23:49:37 -0500

    updated comments in php logger to reflect true use

--- php/log_target.php
+++ php/log_target.php
@@ -14,8 +14,10 @@ if(isset($_REQUEST['worker'])) {
   $targetIdx = $_REQUEST['targetindex'];  // target ID (numeric index of the current target clicked)
   $startTime = $_REQUEST['starttime'];  // start time of the trial (set of targets) or of the previous target hit
   $duration = $_REQUEST['duration'];  // time taken to click target (since starttime)
-  $stLoc = $_REQUEST['startloc'];  // location of mouse pointer [x,y] at start of task (target appears, or prior target hit)
-  $endLoc = $_REQUEST['endloc'];  // location of the mouse pointer [x,y] at the end of the task (target hit)
+  $stLoc = $_REQUEST['startloc'];  // location of target [x,y] at start of task (target appears, or prior target hit)
+  $endLoc = $_REQUEST['endloc'];  // location of the target [x,y] at the end of the task (target hit)
+  //$stLocMouse = $_REQUEST['startlocm'];  // location of mouse pointer [x,y] at start of task (target appears, or prior target hit)
+  //$endLocMouse = $_REQUEST['endlocm'];  // location of the mouse pointer [x,y] at the end of the task (target hit)
   $path = $_REQUEST['path'];  // string containing the all of the locations the mouse was in between the start and the end of this task (JSON list)
   $dist = $_REQUEST['distance'];  // distance between starting mouse location and ending mouse location
   $prox = $_REQUEST['proximity'];  // how close to the center of the target the final (hit) click was
