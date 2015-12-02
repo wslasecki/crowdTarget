7160822e8fa58d7bfae09571543400013416c1de
ElliotSalisbury <elliot.sal@gmail.com>
2015-12-01 20:09:07 -0500

    url param for frameDuration

--- scripts/page.js
+++ scripts/page.js
@@ -1,7 +1,7 @@
 //pixels per second
 var speeds = [50,100,150,200,250,300];
 var numTargets = [1,2,3,4,5,6];
-var stillFrameDuration = 2000;
+var stillFrameDuration = parseInt(gup("frameDuration"));
 var animationFunction=stillFrameAnimation;
 if (stillFrameDuration == 0) {
 	animationFunction=videoAnimation;
